0c9619c32416e3cb1a15ac4e83865c01d315be18
poksiwakorn <62551895+poksiwakorn@users.noreply.github.com>
2022-04-04 00:01:33 +0700

    add SoftCalculator to SoftStudio

---
+++ SoftCalculator/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SoftCalculator.Models;
+
+namespace SoftCalculator.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Calculator()
+    {
+        return View();
+    }
+
+    public JsonResult Calculate(string firstNum = "0", string secondNum = "0", string operand = "+"){
+        double num1 = double.Parse(firstNum);
+        double num2 = double.Parse(secondNum);
+        double result = 0;
+
+        switch(operand){
+            case "+":
+                result = Math.Round(num1 + num2,8);
+                break;
+            case "-":
+                result = Math.Round(num1 - num2,8);
+                break;
+            case "x":
+                result = Math.Round(num1 * num2,8);
+                break;
+            case "/":
+                result = Math.Round(num1 / num2,8);
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        return Json(result);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+
+}
---
+++ SoftCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SoftCalculator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
